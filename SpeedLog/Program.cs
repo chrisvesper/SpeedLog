a58f5f0faf3b8d756511432209f0b2e02fd7752b
Chris Vesper <chrisv@bblsystems.com>
2014-08-21 10:05:55 -0400

    Log in standard date format, and let key interrupt wait

--- SpeedLog/Program.cs
+++ SpeedLog/Program.cs
@@ -13,64 +13,90 @@ namespace SpeedLog
 
             Stopwatch stopWatch = new Stopwatch();
             string result;
-            ConsoleKey keyPressed;
+            ConsoleKeyInfo keyPressed;
 
             do
             {
-                while (!Console.KeyAvailable)
+                using (WebClient Client = new WebClient())
                 {
-                    using (WebClient Client = new WebClient())
+                    try
                     {
-                        try
-                        {
-                            stopWatch.Start();
-                            Client.DownloadString("http://www.google.com/images/google_favicon_128.png");
-                            stopWatch.Stop();
-                            TimeSpan ts1 = stopWatch.Elapsed;
+                        stopWatch.Start();
+                        Client.DownloadString("http://www.google.com/images/google_favicon_128.png");
+                        stopWatch.Stop();
+                        TimeSpan ts1 = stopWatch.Elapsed;
 
-                            stopWatch.Restart();
-                            Client.DownloadString("http://www.bing.com/s/a/bing_p.ico");
-                            stopWatch.Stop();
-                            TimeSpan ts2 = stopWatch.Elapsed;
+                        stopWatch.Restart();
+                        Client.DownloadString("http://www.bing.com/s/a/bing_p.ico");
+                        stopWatch.Stop();
+                        TimeSpan ts2 = stopWatch.Elapsed;
 
-                            result = String.Format("{0:MM/dd/yy H:mm:ss} {1:00}ms,{2:00}ms", DateTime.Now, ts1.Milliseconds, ts2.Milliseconds);
-                        }
-                        catch
-                        {
-                            result = String.Format("{0:MM/dd/yy H:mm:ss} timeout", DateTime.Now);
-                        }
-
-                        try
-                        {
-                            using (StreamWriter file = File.AppendText(@"c:\temp\speedlog.csv"))
-                            {
-                                file.WriteLine(result);
-                                Console.WriteLine(result);
-                            }
-                        }
-                        catch
-                        {
-                            Console.WriteLine(String.Format("{0:MM/dd/yy H:mm:ss} error writing to log file", DateTime.Now));
-                        }
-
+                        result = String.Format(@"{0:yyyy-MM-ddTHH:mm:ss.fffffffzz},{1:00}ms,{2:00}ms", DateTime.Now, ts1.Milliseconds, ts2.Milliseconds);
+                    }
+                    catch
+                    {
+                        result = String.Format(@"{0:yyyy-MM-ddTHH:mm:ss.fffffffzz},timeout", DateTime.Now);
                     }
 
-                    System.Threading.Thread.Sleep(10000);
-                }  //while no key pressed
+                    try
+                    {
+                        using (StreamWriter file = File.AppendText(@"c:\temp\speedlog.csv"))
+                        {
+                            file.WriteLine(result);
+                            Console.WriteLine(result);
+                        }
+                    }
+                    catch
+                    {
+                        Console.WriteLine(String.Format("{0:MM/dd/yy H:mm:ss} error writing to log file", DateTime.Now));
+                    }
 
-                // See what key they pressed
-                keyPressed = Console.ReadKey(true).Key;
+                }
 
-                if (keyPressed == ConsoleKey.Spacebar)
+                keyPressed = ConsoleHelper.ReadKeyWithTimeOut(10000);
+
+                if (keyPressed.Key == ConsoleKey.Spacebar)
                 {
                     Console.WriteLine("Paused, any key to restart...");
                     while (!Console.KeyAvailable)
                     { }
                 }
 
-            } while (keyPressed != ConsoleKey.Escape);
+            } while (keyPressed.Key != ConsoleKey.Escape);
         }
     }
 
+    // From http://hen.co.za/blog/2011/07/console-readkey-with-timeout/
+    public static class ConsoleHelper
+    {
+
+        public static ConsoleKeyInfo ReadKeyWithTimeOut(int timeOutMS)
+        {
+
+            DateTime timeoutvalue = DateTime.Now.AddMilliseconds(timeOutMS);
+
+            while (DateTime.Now < timeoutvalue)
+            {
+
+                if (Console.KeyAvailable)
+                {
+
+                    ConsoleKeyInfo cki = Console.ReadKey();
+                    return cki;
+
+                }
+                else
+                {
+
+                    System.Threading.Thread.Sleep(100);
+
+                }
+
+            }
+            return new ConsoleKeyInfo('A', ConsoleKey.A, false, false, false);
+
+        }
+
+    }
 }
 
